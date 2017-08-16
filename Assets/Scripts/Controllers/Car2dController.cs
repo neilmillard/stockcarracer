e0b640a7e411693b4223b6bb5f5814ebb5b63718
neilmillard <neil@neilmillard.com>
2017-08-16 20:57:16 +0100

    prototype

--- Assets/Scripts/Controllers/Car2dController.cs
+++ Assets/Scripts/Controllers/Car2dController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Car2dController : MonoBehaviour {
 
+	float speedForce = 10f;
+	float turnforce = -200f;
+	float driftFactorGrip = 0.9f;
+	float driftFactorSkid = 1f;
+	float maxGripVelocity = 4.5f;
+	float minGripVelocity = 1.0f; // when the skid stops
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +18,41 @@ public class Car2dController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+
+		Debug.Log (RightVelocity ().magnitude);
+
+		float driftFactor = driftFactorGrip;
+		if (RightVelocity ().magnitude > maxGripVelocity) {
+			driftFactor = driftFactorSkid;
+		}
+
+		if (Input.GetButton ("Accelerate")) {
+			rb.AddForce ( transform.up * speedForce );
+		}
+			
+		if (Input.GetButton ("Brake")) {
+			float speedChange = -speedForce;
+			if (ForwardVelocity ().magnitude < 0f) {
+				speedChange = speedChange / 2;
+			}
+			rb.AddForce (transform.up * speedChange);
+		}
+
+		// angular velocity only effective above ForwardVelocity
+		float tf = Mathf.Lerp(0, turnforce, rb.velocity.magnitude /2 );
+		rb.angularVelocity = Input.GetAxis ("Horizontal" ) * tf;
+
+		rb.velocity = ForwardVelocity () + RightVelocity()*driftFactor ;
+	}
+
+	Vector2 ForwardVelocity() {
+	// how much rigidbody velocity is in the model up direction
+		return transform.up * Vector2.Dot ( GetComponent<Rigidbody2D>().velocity, transform.up );
+	}
+
+	Vector2 RightVelocity() {
+		// how much rigidbody velocity is in the model right direction
+		return transform.right * Vector2.Dot ( GetComponent<Rigidbody2D>().velocity, transform.right );
 	}
 }
